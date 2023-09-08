8924df4781fc790d9582d10aebf314b2738cbf3c
James Hallberg <Grebllah81788@gmail.com>
2023-09-08 13:46:15 -0400

    complete react jsx lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>35 years old, student at Flatiron School</p>
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
